378fee854da172f47b9c0c95b6a5efe1675efcf0
Ryan Singh <ryan25ne@gmail.com>
2026-03-09 23:32:03 +0530

    Time: 0 ms (100%), Space: 19.4 MB (62%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,42 +11,38 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode*ans=nullptr;
-        ListNode*ptr1=list1;
-        ListNode*ptr2=list2;
-        if(list1==nullptr){
-            return list2;
-        }
-        else if(list2==nullptr){
-            return list1;
-        }
         
-        if(list1->val<=list2->val){
-            ans=list1;
-            ptr1=ptr1->next;
+    // Edge cases: if one list is empty, return the other
+    if (!list1) return list2;
+    if (!list2) return list1;
+
+    // 1. Pick the starting head
+    ListNode* head; 
+    if (list1->val <= list2->val) {
+        head = list1;
+        list1 = list1->next;
+    } else {
+        head = list2;
+        list2 = list2->next;
+    }
+
+    // 2. 'prev' is our stitching needle
+    ListNode* prev = head;
+
+    while (list1 && list2) {
+        if (list1->val <= list2->val) {
+            prev->next = list1;   // Stitch
+            list1 = list1->next;  // Move list1 forward
+        } else {
+            prev->next = list2;   // Stitch
+            list2 = list2->next;  // Move list2 forward
         }
-        else{
-            ans=list2;
-            ptr2=ptr2->next;
-        }
-        ListNode*temp=ans;
-        while(ptr1!=nullptr && ptr2!=nullptr){
-            if (ptr1->val<=ptr2->val){
-                temp->next=ptr1;
-                ptr1=ptr1->next;
-            }
-            else{
-                temp->next=ptr2;
-                ptr2=ptr2->next;
-            }
-            temp=temp->next;
-        }
-        if(ptr1==nullptr){
-                temp->next=ptr2;
-        }
-        else{
-                temp->next=ptr1;
-        }
-    return ans;
+        prev = prev->next;        // Move needle forward
+    }
+
+    // 3. Attach the remaining nodes
+    prev->next = list1 ? list1 : list2;
+
+    return head;
     }
 };
