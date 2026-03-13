b0630335d7f1c75b55ea32333e853b7a6b2d98d8
Ryan Singh <ryan25ne@gmail.com>
2026-03-13 22:53:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+
+        if(target<matrix[0][0]){
+            return false;
+        }   
+        if(target>matrix[m-1][n-1]){
+            return false;
+        } 
+        int row=0;
+        while(target>matrix[row][n-1] &&row<m){
+            row++;
+        }
+        int start=0,end=n-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            int curr=matrix[row][mid];
+            if(curr==target){
+                return true;
+            }
+            else if(curr<target){
+                start=mid;
+            }
+            else{
+                end=mid;;
+            }
+        }
+        return false;  
+    }
+};
