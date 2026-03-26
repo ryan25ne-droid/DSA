fdca3ee9e5684824ca9029b4b6d20397fb17c5ad
Ryan Singh <ryan25ne@gmail.com>
2026-03-26 12:26:58 +0530

    Time: 0 ms (100%), Space: 18 MB (16.08%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -3,16 +3,12 @@ public:
     string largestOddNumber(string str) {
         int n=str.size();
         string substr="";
-        string evenseen="";
-        for(int i=0;i<n;i++){
-            if(str[i]=='1' ||str[i]=='3'||str[i]=='5'||str[i]=='7'|| str[i]=='9'){substr+=evenseen;
-            substr+=str[i]; 
-            evenseen="";               
-            }
-            else{
-                evenseen+=str[i];
+        int lastodd=-1;
+        for(int i=n-1;i>=0;i--){
+            if(str[i]=='1' ||str[i]=='3'||str[i]=='5'||str[i]=='7'|| str[i]=='9'){lastodd=i;
+        break;
             }
         }
-        return substr;                
+        return str.substr(0,lastodd+1);                
     }
 };
