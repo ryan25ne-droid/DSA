6a3b57e49b8895d6cdb22dc542f16e06da8ac124
Ryan Singh <ryan25ne@gmail.com>
2026-04-10 23:25:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,56 @@
+class Node{
+public:
+    unordered_map<char,Node*>children;
+    bool endOfword;
+    Node(){
+        endOfword=false;
+    }
+};
+class Trie{
+public:
+    Node*root;
+    Trie(){
+        root=new Node();
+    }
+    void insert(string key){
+        Node*temp=root;
+        for(int i=0;i<key.size();i++){
+            if(temp->children.count(key[i])==0){
+                temp->children[key[i]]=new Node;
+            }
+            temp=temp->children[key[i]];
+        }
+        temp->endOfword=true;
+    }
+    bool search(string key){
+        Node*temp=root;
+        for(int i=0;i<key.size();i++){
+            if(temp->children.count(key[i])==0){
+                return false;
+            }
+            temp=temp->children[key[i]];
+        }
+        return temp->endOfword;
+    }
+};
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans="";
+        Trie trie;
+        for(auto ele:strs){
+            trie.insert(ele);
+        }
+        string key=strs[0];
+        Node*temp=trie.root;
+        int i=0;
+        while(temp->children.size()==1 &&temp->endOfword!=true){
+            temp=temp->children[key[i]];
+            i++;
+            ans+=key[i];
+        }
+
+        return ans;
+    }
+};
