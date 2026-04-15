0f535c93352b049268e36e9b001789658fbcfffa
Ryan Singh <ryan25ne@gmail.com>
2026-04-15 18:44:38 +0530

    Time: 111 ms (65.37%), Space: 17.5 MB (58.71%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,44 +1,35 @@
 class Solution {
 public:
-    bool helper(vector<int>&nums, int n, int sum,vector<vector<int>>&dp){
-    //base case
-        if(n<=0){
-            return false;
-        }
-        if(sum==0){
-            return true;
-        }
-        if(dp[n][sum]!=-1){
-            return dp[n][sum];
-        }
-        int currval=nums[n-1];
-        
-        if(currval<=sum){
-    //include
-            bool case1= helper(nums,n-1,sum-currval,dp);            
-    //exclude
-            bool case2= helper(nums,n-1,sum,dp);
-            dp[n][sum]=(case1||case2);
-        } 
-        else{
-    //exclude
-            dp[n][sum]= helper(nums,n-1,sum,dp);
-        } 
-        return dp[n][sum];      
-    }
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int sum=0;
-        for(int i=0;i<n; i++){
+        for(int i=0;i<n;i++){
             sum+=nums[i];
-        }
+        }  
         if(sum%2!=0){
             return false;
-        }
+        } 
         sum=sum/2;
-        vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-        return helper(nums,n,sum,dp);               
+        vector<vector<bool>>dp(n+1,vector<bool>(sum+1,false));
+//here dp[i][j] represents whether its possible to obtain sum j using any subset of 1st i elements of the array. 
+        
+        //Base case. Sum j=0 is always achievable
+        for(int i=0; i<n+1; i++){
+            dp[i][0]=true;
+        }
+
+        for(int i=1; i<=n; i++){  //i keeps track of no of elements used.
+            int currval= nums[i-1];
+            for(int s=1; s<=sum; s++){  //s keeps track of the sum still left for us to make using elements
+                if(currval <=s){
+                    dp[i][s] = dp[i-1][s] || dp[i-1][s-currval];
+//include- dp[i-1][s-currval],  exclude- dp[i-1][s]
+                } else {
+//exclude
+                    dp[i][s] = dp[i-1][s];
+                }
+            }
+        }
+        return dp[n][sum];
     }
 };
-
-//so the problem in above code is that when i initialise dp with false values and check dp[n][sum]!=false, I imply that if it is false, don't use the dp array. but there is a possibility that the answer for that recursive call has already been calculalted and stored in dp, and now I am not sure whether a false in dp is beccause the given subproblem can't be solved, or whether there hasn't been any attempt to solve it till now
