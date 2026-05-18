02f4496a27fbc5b2c7a54f13559514b77a90b32e
Ryan Singh <ryan25ne@gmail.com>
2026-05-18 15:50:28 +0530

    Time: 22 ms (82.28%), Space: 27.3 MB (66.72%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
-    int helper(string &text1, string &text2, vector<vector<int>>&dp,int n,int m){
-        if(n==0 || m==0){
-            return 0;
-        }
-//DP reusing older computed methods 
-        if(dp[n][m]!=-1){
-            return dp[n][m];
-        }
-
-        if(text1[n-1]==text2[m-1]){
-            dp[n][m]= helper(text1,text2,dp,n-1,m-1)+1;
-            return dp[n][m];
-        }
-        else{
-            int ans1= helper(text1,text2,dp,n,m-1);
-            int ans2= helper(text1,text2,dp,n-1,m);
-            dp[n][m]= max(ans1,ans2);
-            return dp[n][m];
-        }
-    }
-
-    int longestCommonSubsequence(string text1, string text2){
+    int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return helper(text1,text2,dp,n,m);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+//dp[i][j] represents max length of a subsequence while considering the first n and m indexes of the strings. Last exclusive.
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j]= dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+                }                
+            }
+        }
+
+        return dp[n][m];        
     }
 };
-
-//The issue is everytime you use function substring, you allocate fresh memory for that substirng. Which is the reason why you get Time Memory exceeded. Instead use indexes to include/exclude the last character of the string without using the function substring.
