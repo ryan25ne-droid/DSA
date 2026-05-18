2e949c012b536643e2cc32b8d329634c0083481e
Ryan Singh <ryan25ne@gmail.com>
2026-05-18 19:24:22 +0530

    Time: 79 ms (80.22%), Space: 13.3 MB (91.54%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,35 +1,19 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int n=nums.size();
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-        }  
-        if(sum%2!=0){
-            return false;
-        } 
-        sum=sum/2;
-        vector<vector<bool>>dp(n+1,vector<bool>(sum+1,false));
-//here dp[i][j] represents whether its possible to obtain sum j using any subset of 1st i elements of the array. 
-        
-        //Base case. Sum j=0 is always achievable
-        for(int i=0; i<n+1; i++){
-            dp[i][0]=true;
-        }
+        int total = 0;
+        for(int num : nums) total += num;
+        if(total % 2 != 0) return false;
 
-        for(int i=1; i<=n; i++){  //i keeps track of no of elements used.
-            int currval= nums[i-1];
-            for(int s=1; s<=sum; s++){  //s keeps track of the sum still left for us to make using elements
-                if(currval <=s){
-                    dp[i][s] = dp[i-1][s] || dp[i-1][s-currval];
-//include- dp[i-1][s-currval],  exclude- dp[i-1][s]
-                } else {
-//exclude
-                    dp[i][s] = dp[i-1][s];
-                }
+        int target = total / 2;
+        vector<bool> dp(target+1, false);
+        dp[0] = true; // sum 0 is always achievable
+
+        for(int num : nums){
+            for(int s = target; s >= num; s--){
+                dp[s] = dp[s] || dp[s - num];
             }
         }
-        return dp[n][sum];
+        return dp[target];
     }
 };
