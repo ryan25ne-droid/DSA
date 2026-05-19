b07c8b35025aa3c3e25920d791f1aff9405472e0
Ryan Singh <ryan25ne@gmail.com>
2026-05-19 17:45:39 +0530

    Time: 59 ms (59.97%), Space: 14.4 MB (42.22%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,35 @@
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int>dp(n+1,0);
+//         //dp[i] denotes the length of largest subsequence upto idx i.
+//         dp[0]=1;
+//         for(int i=0;i<n-1;i++){
+//             if(nums[i]<nums[i+1]){
+//                 dp[i+1]=dp[i]+1;
+//             }
+//             else{
+//                 dp[i+1]= dp[i];
+//             }
+//         }
+//         return dp[n];
+//     }
+// };
+class Solution{
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        int ans = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+};
