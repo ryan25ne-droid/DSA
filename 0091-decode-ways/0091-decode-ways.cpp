19d5f93575a2e8c1de6c551354a3426b345d2f19
Ryan Singh <ryan25ne@gmail.com>
2026-05-21 14:49:41 +0530

    Time: 0 ms (100%), Space: 9.1 MB (33.08%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,43 +1,31 @@
 class Solution {
 public:
-    int helper(string s, int n, vector<int>&dp){
-        if(s[0]=='0'){  //we have to handle this separately as length of '0' is one and it can return 1 because of other base cases..
-            return 0;
-        }
-
-        dp[0]=1;
-        dp[1]=1;
-
-        if(n==0 || n==1){
-            return 1;
-        }
-
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-
-        char last=s[n-1];
-        int lastTwo= stoi(s.substr(n-2,2));
-//10<=lastTwo is necessary to avoid '00' or'01' or any similar '0X' to pass through
-        if (last!= '0') {
-            dp[n]= helper(s,n-1,dp); // single digit always allowed
-            if (lastTwo >= 10 && lastTwo <= 26) {
-                dp[n]+= helper(s,n-2,dp); // two digit allowed only if 10–26
-            }
-            return dp[n];
-        }
-        else{ //last digit is 0.
-            if (lastTwo==10 || lastTwo==20) {
-                dp[n]= helper(s,n-2,dp);
-                return dp[n];
-            }
-            return 0;
-        }
-    }
-
-    int numDecodings(string s){
+    int numDecodings(string s) {
         int n=s.size();
-        vector<int>dp(n+1,-1);
-        return helper(s,n,dp);               
+        if(s[0]=='0'){
+            return 0;
+        }
+        vector<int>dp(n+1,0); //dp[i] represents no of ways to interpret code upto index i
+        //base case
+        dp[0]=1, dp[1]=1;
+        //dp[0] is necessary because all even strings eventually reduce to it.     
+        for(int i=2;i<n+1;i++){
+            if(s[i-1]!='0'){
+                dp[i]+=dp[i-1];
+                int lastTwo=stoi(s.substr(i-2,2));
+                if(lastTwo>=10 && lastTwo<=26){
+                    dp[i]+=dp[i-2];
+                }
+            }
+            else if(s[i-1]=='0'){
+                if(s[i-2]=='1' || s[i-2]=='2'){
+                    dp[i]+=dp[i-2];
+                }
+                else{ //last 2 digits are 00, 30,40 ...
+                    dp[i]=0;
+                }
+            }
+        }
+        return dp[n];        
     }
 };
