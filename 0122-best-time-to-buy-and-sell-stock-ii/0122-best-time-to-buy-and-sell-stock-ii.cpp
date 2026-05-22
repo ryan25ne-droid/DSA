1bfbe3758a4c67c153849d75f1c2791c542e40b5
Ryan Singh <ryan25ne@gmail.com>
2026-05-22 21:48:34 +0530

    Time: 10 ms (25.89%), Space: 24.7 MB (22.35%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    int helper(vector<int>&prices, vector<vector<int>>&dp, int day, int state, int n){
-        if(dp[day][state]!=-1){
-            return dp[day][state];
-        }
-        if(day==n){
-            return 0;
-        }
-        if(state==0){
-            return dp[day][state]=max(helper(prices,dp,day+1,0,n),helper(prices,dp,day+1,1,n)+prices[day]);
-        }
-        else{ //state=1;
-            return dp[day][state]=max(helper(prices,dp,day+1,1,n), helper(prices,dp,day+1,0,n)-prices[day]);
-        }
-    }
-
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,-1));
-//dp[i][j] stores max profit possible to make on upto day i with state j. j=1 means you can buy stock. j=0 means you can't buy new stock cause you are already holding one. You must sell it.
-        return helper(prices,dp,0,1,n);                
+        vector<vector<int>>dp(n+1,vector<int>(2,-1));
+//dp[i][j] represents max profit possible upto day i and state j.
+        dp[n][0]=dp[n][1]=0;
+        for(int i=n-1;i>=0;i--){
+            dp[i][0]=max(dp[i+1][0], dp[i+1][1]+prices[i]);
+            dp[i][1]=max(dp[i+1][1],dp[i+1][0]-prices[i]);
+        } 
+        return dp[0][1];        //start at day 0, allowed to buy       
     }
 };
