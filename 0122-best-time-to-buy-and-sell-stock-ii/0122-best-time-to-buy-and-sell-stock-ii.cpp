06d555837b64aeb878514d63d3ddf5886f36a508
Ryan Singh <ryan25ne@gmail.com>
2026-05-22 21:06:51 +0530

    Time: 11 ms (24.71%), Space: 27.7 MB (6.53%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
+    int helper(vector<int>&prices, vector<vector<int>>&dp, int day, int state, int n){
+        if(dp[day][state]!=-1){
+            return dp[day][state];
+        }
+        if(day==n){
+            return 0;
+        }
+        if(state==0){
+            return dp[day][state]=max(helper(prices,dp,day+1,0,n),helper(prices,dp,day+1,1,n)+prices[day]);
+        }
+        else{ //state=1;
+            return dp[day][state]=max(helper(prices,dp,day+1,1,n), helper(prices,dp,day+1,0,n)-prices[day]);
+        }
+    }
+
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();   
-        int profit=0;
-//greedy approach. Total profit is sum of all +ve profits achieved over time by selling as soon as you get a profit, then rebuying the stock on exact same day just in case
-        for(int i=0;i<n-1;i++){
-            if(prices[i]<prices[i+1]){
-                profit+=prices[i+1]-prices[i];
-            }
-        } 
-        return profit;                    
+        int n=prices.size();
+        vector<vector<int>> dp(n+1,vector<int>(2,-1));
+//dp[i][j] stores max profit possible to make on upto day i with state j. j=1 means you can buy stock. j=0 means you can't buy new stock cause you are already holding one. You must sell it.
+        return helper(prices,dp,0,1,n);                
     }
 };
