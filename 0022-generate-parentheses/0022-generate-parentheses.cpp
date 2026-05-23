98f82df08a3ff686f623bf86a513f8a328041bb1
Ryan Singh <ryan25ne@gmail.com>
2026-05-23 13:33:47 +0530

    Time: 2 ms (81.36%), Space: 13 MB (89.23%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    void helper(string &paren, vector<string>& ans, int open, int close) {
-        if (open == 0 && close == 0) {
+    void helper(string &paren, vector<string>& ans, int i, int j) {
+        // Base case: no opens left and no unmatched opens
+        if (i == 0 && j == 0) {
             ans.push_back(paren);
             return;
         }
-        if (open > 0) {
+
+        // If we still have opens left, place '('
+        if (i > 0) {
             paren.push_back('(');
-            helper(paren, ans, open-1, close+1);
+            helper(paren, ans, i-1, j+1);  // one less open, one more unmatched
             paren.pop_back();
         }
-        if (close > 0) {
+
+        // If we have unmatched opens, we can place ')'
+        if (j > 0) {
             paren.push_back(')');
-            helper(paren, ans, open, close-1);
+            helper(paren, ans, i, j-1);    // same opens, one less unmatched
             paren.pop_back();
         }
     }
@@ -20,7 +25,8 @@ public:
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
         string paren;
-        helper(paren, ans, n, 0);
+        helper(paren, ans, n, 0);  // start with n opens, 0 unmatched
         return ans;
     }
 };
+
