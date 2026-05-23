bacf56a34863a2cfac1d5986b30ea9682d085b9f
Ryan Singh <ryan25ne@gmail.com>
2026-05-23 13:44:38 +0530

    Time: 247 ms (5.01%), Space: 167.7 MB (5%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -8,6 +8,9 @@ public:
 
         for(int i=0; i<=n; i++){
             for(int j=0; j<=n; j++){
+                if(i==0 && j==0){
+                    continue;
+                }
                 // if we can place '('
                 if (i>0 &&j<n){
                     for(auto &ele: dp[i-1][j+1]){
