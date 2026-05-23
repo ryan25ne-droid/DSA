53dbac4282ee45f7cc7bed5869ac690198178bee
Ryan Singh <ryan25ne@gmail.com>
2026-05-23 13:43:45 +0530

    Time: 235 ms (5.01%), Space: 167.7 MB (5%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    void helper(string &paren, vector<string>& ans, int i, int j) {
-        // Base case: no opens left and no unmatched opens
-        if (i == 0 && j == 0) {
-            ans.push_back(paren);
-            return;
-        }
-
-        // If we still have opens left, place '('
-        if (i > 0) {
-            paren.push_back('(');
-            helper(paren, ans, i-1, j+1);  // one less open, one more unmatched
-            paren.pop_back();
-        }
-
-        // If we have unmatched opens, we can place ')'
-        if (j > 0) {
-            paren.push_back(')');
-            helper(paren, ans, i, j-1);    // same opens, one less unmatched
-            paren.pop_back();
-        }
-    }
-
     vector<string> generateParenthesis(int n) {
-        vector<string> ans;
-        string paren;
-        helper(paren, ans, n, 0);  // start with n opens, 0 unmatched
-        return ans;
+        vector<vector<vector<string>>> dp(n+1, vector<vector<string>>(n+1));
+//dp[i][j] stores all strings possible when I have i total open brackets and j unmatched open brackets
+        // base case
+        dp[0][0].push_back("");
+
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=n; j++){
+                // if we can place '('
+                if (i>0 &&j<n){
+                    for(auto &ele: dp[i-1][j+1]){
+                        dp[i][j].push_back("(" +ele);
+                    }
+                }
+                // if we can place ')'
+                if(j>0){
+                    for(auto &ele:dp[i][j-1]) {
+                        dp[i][j].push_back(")" +ele);
+                    }
+                }
+            }
+        }
+        return dp[n][0];
     }
 };
-
