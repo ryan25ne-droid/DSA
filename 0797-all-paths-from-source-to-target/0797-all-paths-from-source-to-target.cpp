ab7081bed520ed61af550bf65882244b6951a5d9
Ryan Singh <ryan25ne@gmail.com>
2026-05-28 14:08:32 +0530

    Time: 0 ms (100%), Space: 13.9 MB (57.27%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,29 +1,30 @@
+//We don't need visited array here at all.
+//In problems like “all paths in an undirected graph,” revisiting nodes can cause cycles and duplicate paths.
+
+// So we use visited to prevent revisiting. But here the graph is a DAG. There are no cycles. Every path eventually terminates at n‑1.
+
+// Using visited here is redundant
+
 class Solution {
 public:
-    void helper(int src, int dest, vector<bool>&visited, vector<vector<int>>&adj,vector<vector<int>>&ans, vector<int>&path){
-        visited[src]=true;
+    void helper(int src, int dest, vector<vector<int>>&adj,vector<vector<int>>&ans, vector<int>&path){
         path.push_back(src);
         if(src==dest){
             ans.push_back(path);
             return;
         } 
         for(auto &ele: adj[src]){
-            if(!visited[ele]){
-                helper(ele,dest,visited,adj,ans,path);
-                visited[ele]=false;
-                path.pop_back();
-            }
+            helper(ele,dest,adj,ans,path);
+            path.pop_back();
         } 
     }
 
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& adj){
         int n=adj.size();
-
-        vector<bool>visited(n,false);
         vector<int>path;
         vector<vector<int>>ans;
 
-        helper(0,n-1,visited,adj,ans,path);
+        helper(0,n-1,adj,ans,path);
         return ans;        
     }
 };
