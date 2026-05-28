682ab9c7020695a25dc28f3accf45feb912c2a80
Ryan Singh <ryan25ne@gmail.com>
2026-05-28 14:03:31 +0530

    Time: 3 ms (73.3%), Space: 13.6 MB (98.71%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     void helper(int src, int dest, vector<bool>&visited, vector<vector<int>>&adj,vector<vector<int>>&ans, vector<int>&path){
+        visited[src]=true;
+        path.push_back(src);
         if(src==dest){
             ans.push_back(path);
             return;
         } 
         for(auto &ele: adj[src]){
             if(!visited[ele]){
-                path.push_back(ele);
-                visited[ele]=true;
                 helper(ele,dest,visited,adj,ans,path);
                 visited[ele]=false;
                 path.pop_back();
@@ -21,7 +21,6 @@ public:
 
         vector<bool>visited(n,false);
         vector<int>path;
-        path.push_back(0);
         vector<vector<int>>ans;
 
         helper(0,n-1,visited,adj,ans,path);
