ac97e263147f8414dede93b452cbaf05003c095f
Ryan Singh <ryan25ne@gmail.com>
2026-05-28 11:45:02 +0530

    Time: 0 ms (100%), Space: 21.1 MB (9.61%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-    void helper(vector<vector<int>>&adj, vector<bool>&visited, int src){
-        visited[src]=true;
-        for(auto& v: adj[src]){
-            if(!visited[v]){
-                helper(adj,visited,v);
+    void helper(vector<vector<int>>& adj, vector<bool>& visited, int src) {
+        visited[src] = true;
+        for (auto& v : adj[src]) {
+            if (!visited[v]) {
+                helper(adj, visited, v);
             }
         }
-        return;
     }
 
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        vector<vector<int>>adj (n);
+        int n = isConnected.size();
+        vector<vector<int>> adj(n);
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1) {
                     adj[i].push_back(j);
                     adj[j].push_back(i);
                 }
             }
         }
-        int count=0;
 
-        vector<bool>visited(n,false);
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                helper(adj,visited,i);
+        int count = 0;
+        vector<bool> visited(n, false);
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                helper(adj, visited, i);
                 count++;
             }
         }
-        return count;        
+        return count;
     }
 };
