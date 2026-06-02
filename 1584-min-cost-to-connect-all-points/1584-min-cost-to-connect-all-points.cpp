3ce36c65b6485366f34235e2c302f37dc098aac5
Ryan Singh <ryan25ne@gmail.com>
2026-06-02 22:27:43 +0530

    Time: 25 ms (82.96%), Space: 14.5 MB (85.97%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    int minCostConnectPoints(vector<vector<int>>& points){
-        int V= points.size();
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int V = points.size();
+        vector<int> dist(V, INT_MAX);
+        vector<bool> inMST(V, false);
 
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-
-        vector<bool> MST(V,false);
-        
+        dist[0]=0;
         int ans=0;
 
-        pq.push(make_pair(0,0));  //src=0 vertex with its distance as 0 from itself
+        for(int i=0; i<V; i++){
+            int u =-1;
+            for(int j=0; j<V; j++){
+                if(!inMST[j] && (u==-1 ||dist[j]<dist[u])){
+                    u=j;
+                }
+            }
 
-        while(!pq.empty()){
-            int u= pq.top().second;
-            int cost=pq.top().first;
-            pq.pop();
+            inMST[u] = true;
+            ans+=dist[u];
 
-            if(!MST[u]){
-                MST[u]=true;
-                ans+=cost;
+            for(int v=0; v<V; v++){
+                if(!inMST[v]){
+                    int wt=abs(points[u][0]-points[v][0])+abs(points[u][1] -points[v][1]);
 
-                for(int v=0; v<V; v++){
-                    if(u!=v){
-                        int wt= abs(points[u][0]-points[v][0])+abs(points[u][1]-points[v][1]);
-                        pq.push(make_pair(wt,v));
-                    }
+                    dist[v] = min(dist[v], wt);
                 }
             }
         }
