0f506c5b924baa9d611fd6f3b77f5ba3dc3aba31
Ryan Singh <ryan25ne@gmail.com>
2026-06-02 22:34:11 +0530

    Time: 18 ms (94.74%), Space: 14.6 MB (85.97%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -3,7 +3,7 @@ public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int V = points.size();
         vector<int> dist(V, INT_MAX);
-        vector<bool> inMST(V, false);
+        vector<bool> MST(V, false);
 
         dist[0]=0;
         int ans=0;
@@ -11,16 +11,16 @@ public:
         for(int i=0; i<V; i++){
             int u =-1;
             for(int j=0; j<V; j++){
-                if(!inMST[j] && (u==-1 ||dist[j]<dist[u])){
+                if(!MST[j] && (u==-1 ||dist[j]<dist[u])){
                     u=j;
                 }
             }
 
-            inMST[u] = true;
+            MST[u] = true;
             ans+=dist[u];
 
             for(int v=0; v<V; v++){
-                if(!inMST[v]){
+                if(!MST[v]){
                     int wt=abs(points[u][0]-points[v][0])+abs(points[u][1] -points[v][1]);
 
                     dist[v] = min(dist[v], wt);
@@ -30,3 +30,7 @@ public:
         return ans;
     }
 };
+
+//Sparse graphs (few edges compared to vertices): Using a priority queue (min-heap) is better. It gives 𝑂(𝐸log𝑉), which is efficient when 𝐸 is much smaller than 𝑉^2. That’s why in road networks or graphs with limited connections, the heap-based Prim’s is the right choice.
+
+//Dense graphs (like your LeetCode problem, where every point connects to every other):  Here 𝐸≈𝑉^2. The heap version degenerates to 𝑂(𝑉^2log𝑉). In this case, a simple array-based Prim’s (tracking minDist and scanning linearly) is faster: 𝑂(𝑉^2).
