008e0f34b1fe0d7ce8f275a0341fd780f10cb0d4
Ryan Singh <ryan25ne@gmail.com>
2026-06-03 19:40:00 +0530

    Time: 4 ms (25.81%), Space: 12.8 MB (99.54%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,71 @@
+// classical ques of finding a disjoint set
+class Disjoint_Set{
+public:
+    int n;
+    vector<int>parent;
+    vector<int>rank;
+
+    Disjoint_Set(int n){
+        this->n=n;
+
+        for(int i=0;i<n;i++){  //initialising the parent and rank vectors
+            parent.push_back(i);
+            rank.push_back(0);
+        }
+    }
+
+    int find(int x){
+        if(parent[x]==x){
+            return x;
+        }
+        else{
+            return parent[x]=find(parent[x]);    //path compression
+        }
+    }
+
+    void unionByRank(int a, int b){
+        int parA=find(a);
+        int parB=find(b);
+
+        if(rank[parA]==rank[parB]){
+            parent[parB]=parA;
+            rank[parA]++;
+        }
+        else if(rank[parA]>rank[parB]){
+            parent[parB]=parA;
+        }
+        else{
+            parent[parA]=parB;
+        }
+    }
+    bool findSet(int a, int b){  //do a and b belong in same set?
+        int parA=find(a);
+        int parB=find(b);
+
+        if(parA==parB){
+            return true;
+        }
+        return false;        
+    }
+};
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges){
+        int n=edges.size();
+        Disjoint_Set DJ(n+1);
+
+        for(auto& ele:edges){
+            int u=ele[0];
+            int v=ele[1];
+
+            if(DJ.findSet(u,v)){
+                return ele;
+            }
+            else{
+                DJ.unionByRank(u,v);
+            }
+        }   
+        return {}; //should never reach here iff input graph has a redundant edge
+    }
+};
