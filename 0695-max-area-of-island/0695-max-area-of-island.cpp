82e4b973bb91a89527d7205aaa6cd1a7ed7622a6
Ryan Singh <ryan25ne@gmail.com>
2026-06-04 16:11:16 +0530

    Time: 1 ms (61.89%), Space: 28.1 MB (58.75%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,3 +1,6 @@
+//optimisations in BFS. Move dir array outside the loop to avoid allocatinng and deallocating space repeatedly.
+
+//instead of maintaining a separate array visited, we can mark a cell 1 to 0 after visiting it to ensure we don't ever visit it  again
 class Solution {
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid){
@@ -5,25 +8,25 @@ public:
         int n=grid.size();
         int m=grid[0].size();
         queue<pair<int,int>>q;
-        vector<vector<bool>>visited(n,vector<bool>(m,false));
+        const int dir[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
 
 //initialisation
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1 && !visited[i][j]){
-                    visited[i][j]=true;
+                if(grid[i][j]==1 ){
+                    grid[i][j]=0;
                     q.push(make_pair(i,j));
                     int size=1;
                     while(!q.empty()){
                         int row=q.front().first;
                         int col=q.front().second;
                         q.pop();
-                        int dir[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
+                        
                         for(auto& ele:dir){
                             int nrow=row+ele[0];
                             int ncol=col+ele[1];
-                            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && !visited[nrow][ncol]){
-                                visited[nrow][ncol]=true;
+                            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1){
+                                grid[nrow][ncol]=0;
                                 q.push(make_pair(nrow,ncol));
                                 size++;
                             }
