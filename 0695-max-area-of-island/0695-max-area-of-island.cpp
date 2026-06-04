698bb0d82f9fee63a0c0245538ce1742a299dc07
Ryan Singh <ryan25ne@gmail.com>
2026-06-04 16:05:33 +0530

    Time: 3 ms (53.6%), Space: 28.9 MB (33.96%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
-    int helper(int i, int j, int n, int m, vector<vector<bool>>&visited, vector<vector<int>>& grid){
-
-        visited[i][j]=true;
-        int dir[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
-        int ans=1;  //count current cell
-
-        for(auto&ele: dir){
-            int nrow= i+ele[0];
-            int ncol= j+ele[1];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && !visited[nrow][ncol]){
-                ans+=helper(nrow,ncol,n,m,visited,grid);
-            }
-        }
-        return ans;
-    }
-
     int maxAreaOfIsland(vector<vector<int>>& grid){
+        int ans=0;
         int n=grid.size();
         int m=grid[0].size();
+        queue<pair<int,int>>q;
         vector<vector<bool>>visited(n,vector<bool>(m,false));
 
-        int Max=0;
-
+//initialisation
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!visited[i][j] && grid[i][j]==1){
-                    Max=max(Max, helper(i,j,n,m,visited,grid));
+                if(grid[i][j]==1 && !visited[i][j]){
+                    visited[i][j]=true;
+                    q.push(make_pair(i,j));
+                    int size=1;
+                    while(!q.empty()){
+                        int row=q.front().first;
+                        int col=q.front().second;
+                        q.pop();
+                        int dir[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
+                        for(auto& ele:dir){
+                            int nrow=row+ele[0];
+                            int ncol=col+ele[1];
+                            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && !visited[nrow][ncol]){
+                                visited[nrow][ncol]=true;
+                                q.push(make_pair(nrow,ncol));
+                                size++;
+                            }
+                        }
+                    }
+                    ans=max(ans,size);
                 }
             }
-        }
-        return Max;
+        } 
+        return ans;       
     }
 };
