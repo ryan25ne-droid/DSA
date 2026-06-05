2a997f9148532eb6965fd7a69191146e2387dd3c
Ryan Singh <ryan25ne@gmail.com>
2026-06-05 14:33:52 +0530

    Time: 45 ms (96.93%), Space: 11.9 MB (13.29%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -4,7 +4,6 @@ public:
         if(k==word.size()-1){
             return true;
         }
-        bool ans;
 
         visited[i][j]=true;
         int dir[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
@@ -12,10 +11,9 @@ public:
             int nrow=i +ele[0];
             int ncol= j+ele[1];
             if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && board[nrow][ncol]==word[k+1] && !visited[nrow][ncol]){
-                ans=helper(nrow,ncol,n,m,board,word,visited,k+1);
-                if(ans==true){
-                    return ans;
-                }
+                if(helper(nrow,ncol,n,m,board,word,visited,k+1)){
+                    return true;
+                }                
             }
         }
         visited[i][j]=false;
@@ -25,16 +23,35 @@ public:
     bool exist(vector<vector<char>>& board, string word){
         int n=board.size();
         int m=board[0].size();
+//optimisations
+        if(word.size()>n*m){
+            return false;
+        }
+
+        unordered_map<char,int> freqBoard;
+        for(auto &row:board){
+            for(char c:row){
+                freqBoard[c]++;
+            }
+        }
+        for(char c:word){
+            freqBoard[c]--;
+        }
+        for(char c:word){
+            if(freqBoard[c] <0) {
+                return false;
+            }
+        }
+
         bool found=false;
         vector<vector<bool>> visited(n,vector<bool>(m,false));
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(board[i][j]==word[0]){
-                    found=helper(i,j,n,m,board,word,visited,0);
-                    if(found==true){
+                    if(helper(i,j,n,m,board,word,visited,0)){
                         return true;
-                    }                    
+                    }              
                 }
             }
         } 
