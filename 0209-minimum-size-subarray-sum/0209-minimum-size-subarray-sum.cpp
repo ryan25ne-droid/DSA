7f52d1000d6d58da376f139123aefe8a8b89ae75
Ryan Singh <ryan25ne@gmail.com>
2026-06-05 10:09:20 +0530

    Time: 0 ms (100%), Space: 32.1 MB (73.03%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -2,31 +2,21 @@ class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums){
         int n=nums.size();
-        vector<int>dp(n+1,0); //dp[i] stores sum of indexes (0 to i-1) in prefix array
-        dp[0]=0;
+        int left=0;
+        int sum=0;
+        int minLen= INT_MAX;
 
-        for(int i=1;i<n+1;i++){
-            dp[i]=dp[i-1]+nums[i-1];
+        for(int right=0; right<n ;right++){
+            sum+=nums[right];
+
+            while(sum>=target){
+                minLen=min(minLen, right-left+1);
+                sum-=nums[left];
+                left++;
+            }
         } 
-
-        int minLen=INT_MAX;
-
-        for(int i=0;i<n;i++){
-            int sum= dp[i]+ target;
-            int start=i+1;
-            int end=n;
-
-            while(start<=end){
-                int mid=start+(end-start)/2;
-                if(dp[mid]>=sum){
-                    minLen= min(mid-i,minLen);
-                    end=mid-1;
-                }
-                else{
-                    start=mid+1;
-                }
-            }           
-        } 
-        return minLen==INT_MAX ? 0:minLen;     
+        return minLen==INT_MAX ? 0:minLen;       
     }
 };
+
+//saince all no are +ve, increasing right increases the sum (including more numbers) and increasing left decreases sum (excluding numbers). 
