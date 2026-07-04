be6bbf12417539c2131a6060493fce6c2f2c89a4
Ryan Singh <ryan25ne@gmail.com>
2026-07-04 10:23:11 +0530

    Time: 0 ms (100%), Space: 13.3 MB (86.81%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,18 +1,34 @@
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums){
+//         int ans=0;
+//         for(int bitIdx=0; bitIdx<32; bitIdx++){
+//             int count=0;
+//             for(int i=0;i<nums.size();i++){
+//                 if(nums[i] & (1<<bitIdx)){
+//                     count++;
+//                 }
+//             }
+//             if(count%3 !=0){
+//                 ans= ans | (1<<bitIdx);
+//             }
+//         }  
+//         return ans;      
+//     }
+// };
+
+//its TC= O(32*N)
+
 class Solution {
 public:
     int singleNumber(vector<int>& nums){
-        int ans=0;
-        for(int bitIdx=0; bitIdx<32; bitIdx++){
-            int count=0;
-            for(int i=0;i<nums.size();i++){
-                if(nums[i] & (1<<bitIdx)){
-                    count++;
-                }
+        sort(nums.begin(), nums.end());
+
+        for(int ptr=1;ptr<nums.size(); ptr=ptr+3){
+            if(nums[ptr]!=nums[ptr-1]){
+                return nums[ptr-1];
             }
-            if(count%3 !=0){
-                ans= ans | (1<<bitIdx);
-            }
-        }  
-        return ans;      
+        }    
+        return nums[nums.size()-1];        
     }
 };
