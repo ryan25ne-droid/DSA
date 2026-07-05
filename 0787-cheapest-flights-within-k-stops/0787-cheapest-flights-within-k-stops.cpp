1532866ebf508dc2252aa5572e8eaedde6d322f0
Ryan Singh <ryan25ne@gmail.com>
2026-07-05 14:59:29 +0530

    Time: 0 ms (100%), Space: 17 MB (95.94%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,35 +1,33 @@
+//bellman's Algo. Normal Bellman algo needs us to set an outer loop running n-1 times (n is no of vertices) and an inner loop traversing over all the edges. 
+//We don’t need an extra loop over vertices or over stops inside the edge loop. The "stop count" is implicitly handled by the outer loop count (each iteration corresponds to one more edge allowed).
+
+//So the structure is really just two loops:
+//Outer loop for up to 𝐾+1 relaxations and Inner loop over edges
+
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
-        vector<vector<pair<int,int>>> graph(n);
-        for(auto &ele : flights) {
-            graph[ele[0]].push_back({ele[1], ele[2]});
-        }
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k){
 
-        vector<vector<int>> dist(n, vector<int>(K+2, INT_MAX));
-        dist[src][K+1] = 0;
-
-        priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<>> pq;
-        pq.push({0, src, K+1});
-
-        while(!pq.empty()) {
-            auto [cost, node, stops] = pq.top();
-            pq.pop();
-
-            if(node == dst){
-                return cost;
-            }
-
-            if(stops>0) {
-                for(auto &[next, price] : graph[node]) {
-                    if(cost+price< dist[next][stops-1]) {
-                        dist[next][stops-1]= cost+price;
-                        pq.push({cost+price, next, stops-1});
-                    }
+        vector<int>dist(n,INT_MAX);
+        dist[src]=0;
+        for(int i=0;i<k+1;i++){  //k stops mean traversing k+1 edges
+        vector<int> next=dist;
+            for(auto& ele: flights){
+                int u=ele[0];
+                int v=ele[1];
+                int wt=ele[2];
+                
+                if(dist[u]!=INT_MAX && next[v]>dist[u]+wt){
+                    next[v]=dist[u]+wt;
                 }
-            }
+            } 
+            dist=next;  //this is done so that we don't use the newly updated value of previous edge to update current edge in same iteration, which would result in more than k stops.            
         }
-        return -1;
+
+        if(dist[dst]==INT_MAX){
+            return -1;
+        }
+        return dist[dst];
     }
 };
 
