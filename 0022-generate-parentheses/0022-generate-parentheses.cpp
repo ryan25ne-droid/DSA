78fb57bc02b6db47a820a39d8122de0453ec71e5
Ryan Singh <ryan25ne@gmail.com>
2026-07-10 14:20:42 +0530

    Time: 2 ms (81.84%), Space: 11.3 MB (97.66%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<string> generateParenthesis(int n) {
         vector<vector<vector<string>>> dp(n+1, vector<vector<string>>(n+1));
-//dp[i][j] stores all strings possible when I have i total open brackets and j unmatched open brackets
+//dp[i][j] stores all strings possible when I have used i open brackets and j closed brackets
         // base case
         dp[0][0].push_back("");
 
@@ -11,20 +11,23 @@ public:
                 if(i==0 && j==0){
                     continue;
                 }
-                // if we can place '('
-                if (i>0 &&j<n){
-                    for(auto &ele: dp[i-1][j+1]){
-                        dp[i][j].push_back("(" +ele);
+                if(j>i){
+                    continue;
+                }
+                // if i>0 we can place '('
+                if(i>0){
+                    for(auto &ele: dp[i-1][j]){
+                        dp[i][j].push_back(ele + "(");
                     }
                 }
-                // if we can place ')'
+                // if j>0 we can insert ')'
                 if(j>0){
                     for(auto &ele:dp[i][j-1]) {
-                        dp[i][j].push_back(")" +ele);
+                        dp[i][j].push_back(ele + ")");
                     }
                 }
             }
         }
-        return dp[n][0];
+        return dp[n][n];
     }
 };
