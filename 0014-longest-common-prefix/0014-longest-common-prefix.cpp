38f8fe34edab0d7ab18187a3d5414a9d004e3802
Ryan Singh <ryan25ne@gmail.com>
2026-07-18 21:39:36 +0530

    Time: 0 ms (100%), Space: 12.6 MB (6.23%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,36 +1,25 @@
-class Node{
-public:
+struct Node{
     unordered_map<char,Node*>children;
     bool endOfword;
     Node(){
         endOfword=false;
     }
 };
-class Trie{
-public:
+
+struct Trie{
     Node*root;
     Trie(){
         root=new Node();
     }
     void insert(string key){
-        Node*temp=root;
-        for(int i=0;i<key.size();i++){
+        Node*temp= root;
+        for(int i=0; i<key.size(); i++){
             if(temp->children.count(key[i])==0){
                 temp->children[key[i]]=new Node;
             }
             temp=temp->children[key[i]];
         }
         temp->endOfword=true;
-    }
-    bool search(string key){
-        Node*temp=root;
-        for(int i=0;i<key.size();i++){
-            if(temp->children.count(key[i])==0){
-                return false;
-            }
-            temp=temp->children[key[i]];
-        }
-        return temp->endOfword;
     }
 };
 
@@ -55,9 +44,8 @@ public:
         while(temp->children.size()==1 && temp->endOfword!=true){
             ans+=temp->children.begin()->first;
             temp=temp->children.begin()->second;
-        //temp->chiildren.begin() returns the first element of map. and since map has only one element...
+//temp->children.begin() returns the first element of map. and since map has only one element...
         }
-
         return ans;
     }
 };
