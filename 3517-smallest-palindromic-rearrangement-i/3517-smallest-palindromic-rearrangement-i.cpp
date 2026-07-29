8d197e7abc2e349ff002e0d03562b421ef3f7189
Ryan Singh <ryan25ne@gmail.com>
2026-07-29 12:09:34 +0530

    Time: 30 ms (68.98%), Space: 75.7 MB (35.14%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,54 +1,30 @@
 class Solution {
 public:
     string smallestPalindrome(string s){
-        unordered_map<char, int> mp;
+        int arr[26]={0};
         for(int i=0; i<s.size(); i++){
-            mp[s[i]]++;
+            arr[s[i]-'a']++;
         }
 
-        char ptr='a';
+        string special="";
         string first= "";
 
-        if(s.size()%2==0){  //even str case
-            while(ptr<='z'){
-                if(mp.count(ptr)!=0){
-                    int count= mp[ptr]/2;
-                    while(count>0){
-                        first+= ptr;
-                        count--;
-                    }
+        for (int i=0; i<26; i++){
+            if(arr[i]>0){
+                if(arr[i]%2==0){
+                    int count= arr[i]/2;
+                    first.append(count, 'a'+i);
                 }
-                ptr++;
+                else{
+                    int count= arr[i]/2;
+                    first.append(count, 'a'+i);
+                    special+= 'a'+i;
+                }
             }
-            string second= first;
-            reverse(second.begin(), second.end());
-            return first+second;
         }
-        else{  //odd str case
-            string special="";
-            while(ptr<='z'){
-                if(mp.count(ptr)!=0 && mp[ptr] %2==0){
-                    int count= mp[ptr]/2;
-                    while(count>0){
-                        first+= ptr;
-                        count--;
-                    }
-                }
-                else if(mp.count(ptr)!=0 && mp[ptr] %2!=0){
-                    int count= mp[ptr]/2;
-                    while(count>0){
-                        first+=ptr;
-                        count--;
-                    }
-                    special.push_back(ptr);
-                }
-                ptr++;
-            }
-            string second= first;
-            reverse(second.begin(), second.end());
-            return first+special+second;
-        }
-        return "";
+        string second= first;
+        reverse(second.begin(), second.end());
+        return first+special+second;
     }
 };
 
