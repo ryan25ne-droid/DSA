66ba69c66f76a3b90a3dea974e5beec4c05ec61a
Ryan Singh <ryan25ne@gmail.com>
2026-08-02 09:46:27 +0530

    Time: 103 ms (34.54%), Space: 76.7 MB (14.7%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -8,23 +8,14 @@ public:
 // mpp stores (value, freq)
 
         for(int i=0; i<n; i++){
-            mpp[nums[i]]++;
-        }
-        
-        for(int i=0; i<n; i++){
             int target= k- nums[i];
-            if(target== nums[i]){
-                ans+= mpp[target]/2;
-                mpp[target]=0;
-            }else{
-                if(mpp[target]>0){
-                    int count= min(mpp[target], mpp[nums[i]]);
-                    ans+= count;
-                    mpp[target]-= count;
-                    mpp[nums[i]]-=count;
-                }
+            if(mpp[target]>0){
+                mpp[target]--;
+                ans++;
             }
-
+            else{
+                mpp[nums[i]]++;
+            }
         }
         return ans;
     }
