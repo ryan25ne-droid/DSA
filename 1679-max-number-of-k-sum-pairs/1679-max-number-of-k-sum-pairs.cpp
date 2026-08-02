e0b2290c0d4ee1c07ceac2eb3cc87841be7e2d42
Ryan Singh <ryan25ne@gmail.com>
2026-08-02 09:48:57 +0530

    Time: 90 ms (62.79%), Space: 61.9 MB (88.32%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,22 +1,29 @@
+// try using 2 pointers and sorting, or hash map
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k){
         int n= nums.size();
-        unordered_map<int, int> mpp;
-        int ans=0;
+        int ans=0; 
+        
+        sort(nums.begin(), nums.end());
+        int start=0;
+        int end=n-1;
 
-// mpp stores (value, freq)
-
-        for(int i=0; i<n; i++){
-            int target= k- nums[i];
-            if(mpp[target]>0){
-                mpp[target]--;
+        while(start<end){
+            int sum= nums[start]+nums[end];
+            if(sum== k){
                 ans++;
+                start++;
+                end--;
+            }
+            else if(sum< k){
+                start++;
             }
             else{
-                mpp[nums[i]]++;
+                end--;
             }
         }
+        
         return ans;
     }
 };
