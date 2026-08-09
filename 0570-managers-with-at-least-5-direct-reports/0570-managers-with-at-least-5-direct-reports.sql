96b4498b0cbcb957513b75b22900327957c12062
Ryan Singh <ryan25ne@gmail.com>
2026-08-09 23:31:02 +0530

    Time: 407 ms (25.86%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+
+SELECT e1.name AS name
+FROM Employee e1
+JOIN (
+    SELECT e.managerId AS mId,
+    COUNT(*) AS cnt
+    FROM Employee e
+    GROUP BY e.managerId
+) AS e2
+
+ON e1.id= e2.mId
+WHERE e2.cnt>=5
