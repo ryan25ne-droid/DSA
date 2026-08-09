739b86478a66012a0c4f032c2660877e4e209cdb
Ryan Singh <ryan25ne@gmail.com>
2026-08-09 23:01:45 +0530

    Time: 1927 ms (5%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,17 +1,13 @@
 # Write your MySQL query statement below
 
 WITH combTable AS (
-    SELECT e.id AS empId,
+    SELECT d.name as Dep, e.name as Emp, e.salary AS sal,
     DENSE_RANK() OVER (Partition BY d.name ORDER BY e.salary DESC) AS rnk
     FROM Employee e
     JOIN Department d
     ON e.departmentId= d.id
 )
 
-SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
-FROM Employee e
-JOIN Department d
-ON e.departmentId= d.id
-JOIN combTable c
-ON e.id= c.empId
-WHERE c.rnk<=3;
+SELECT c.Dep AS Department, c.Emp as Employee, c.sal AS Salary
+FROM combTable c
+WHERE rnk<=3;
