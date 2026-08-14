1c095c9946def545f92fc78f02fc855e459e34a3
Ryan Singh <ryan25ne@gmail.com>
2026-08-14 11:19:00 +0530

    Time: 6 ms (88.12%), Space: 9.5 MB (57.46%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        int n=s.size();
-        if (n==0){
-            return "";
-        }
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        int start=0, maxLen=1;
-//dp[i][j] tracks whether the string b/w indexes i and j is a substring or not. Instead of using substrings which we know take a lot of memory, use indexes
+    string longestPalindrome(string s){
+        int n= s.size();
+        int maxLen= 1;
+        int start=0;
 
-        //Base case: single characters for odd palindromes
         for(int i=0; i<n; i++){
-            dp[i][i]=true;
-        }
+            int len= 1;
+            int left= i-1;
+            int right= i+1;
+// odd palindrome
+            while(left>=0 && right<n && s[left]== s[right]){
+                len+=2;
+                right++;
+                left--;
+            }
+            if(maxLen< len){
+                maxLen= len;
+                start= left+1;
+            }
+        } 
 
-        //Base case: two characters for even palindromes
-        for(int i=0; i<n-1; i++) {
-            if (s[i]== s[i+1]){
-                dp[i][i+1]=true;
-                start=i;
-                maxLen=2;
+        for(int i=0; i<n; i++){
+            int len=0;
+            int left=i; 
+            int right= i+1;
+            while(left>=0 && right<n && s[left]== s[right]){
+                len+=2;
+                right++;
+                left--;
+            }
+            if(maxLen< len){
+                maxLen= len;
+                start= left+1;
             }
         }
-
-        // Length >= 3
-        for (int len=3; len<=n; len++) {
-            for (int i=0; i<=n-len; i++) {
-                int j=i+len-1;
-                if (s[i]==s[j] && dp[i+1][j-1]) {
-                    dp[i][j] = true;
-                    if (len>maxLen) {
-                        start=i;
-                        maxLen=len;
-                    }
-                }
-            }
-        }
-        return s.substr(start, maxLen);
+        return s.substr(start, maxLen);     
     }
 };
