31f795e102abbbc002d11bbd78250f8ca33e4b72
Ryan Singh <ryan25ne@gmail.com>
2026-09-12 08:43:33 +0530

    Time: 43 ms (97.67%), Space: 11.8 MB (18.31%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
-    bool helper(int i, int j,int n, int m,vector<vector<char>>&board,string &word,vector<vector<bool>>&visited, int k){
-        if(k==word.size()-1){
+    bool helper(int i, int j,int n, int m,vector<vector<char>>&board,string &word, int k){
+
+        if(k== word.size()-1){
             return true;
         }
 
-        visited[i][j]=true;
-        int dir[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
-        for(auto&ele: dir){
+        char original= board[i][j];
+        board[i][j]= '#';
+        int dir[4][2]= {{0,1}, {1,0}, {0,-1}, {-1,0}};
+
+        for(auto& ele: dir){
             int nrow=i +ele[0];
             int ncol= j+ele[1];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && board[nrow][ncol]==word[k+1] && !visited[nrow][ncol]){
-                if(helper(nrow,ncol,n,m,board,word,visited,k+1)){
+
+            if (nrow>=0 && nrow<n && ncol>=0 && ncol<m && board[nrow][ncol]==word[k+1] && board[nrow][ncol]!= '#'){
+                if(helper(nrow, ncol, n, m, board, word, k+1)){
                     return true;
                 }                
             }
         }
-        visited[i][j]=false;
+        board[i][j]= original;
         return false;
     }
 
     bool exist(vector<vector<char>>& board, string word){
         int n=board.size();
         int m=board[0].size();
+
 //optimisations
-        if(word.size()>n*m){
+        if(word.size() >n*m){
             return false;
         }
 
@@ -43,18 +48,18 @@ public:
             }
         }
 
-        bool found=false;
-        vector<vector<bool>> visited(n,vector<bool>(m,false));
+        bool found= false;
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(board[i][j]==word[0]){
-                    if(helper(i,j,n,m,board,word,visited,0)){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(board[i][j]== word[0]){
+                    if(helper(i, j, n, m, board, word, 0)){
                         return true;
                     }              
                 }
             }
         } 
+        
         return false;       
     }
 };
